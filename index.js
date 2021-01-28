b324207c6a4e5552e4ec2b5e2c17003c84428567
Terrence Wong <terrencewong223@gmail.com>
2021-01-27 16:10:20 -0800

    fixed module.exports

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
-function isKenny(input) {
+module.exports = function isKenny(input) {
   if (typeof input !== 'string') {
     return false;
   }
   return input.toLowerCase() === 'kenny' ? true : false;
 }
-
-module.exports.isKenny = isKenny;
