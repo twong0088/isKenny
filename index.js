e83e4947ea48bc61bc369fc7b1a26d8f63eb56bd
Terrence Wong <terrencewong223@gmail.com>
2021-01-28 20:14:51 -0800

    cleaned up code

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ module.exports = function isKenny(input) {
   if (typeof input !== 'string') {
     return false;
   }
-  return input.toLowerCase() === 'kenny' ? true : false;
+  return input.toLowerCase() === 'kenny';
 }
